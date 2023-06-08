a0e6f440ebf0f6be0ad518deccdbffb3443e2cdf
tzh <284171810@qq.com>
2023-06-08 18:37:45 +0800

    ccc

--- keplar/operator/reinserter.py
+++ keplar/operator/reinserter.py
@@ -1,3 +1,5 @@
+import random
+
 from keplar.operator.operator import Operator
 import pyoperon as Operon
 
@@ -9,31 +11,45 @@ class Reinserter(Operator):
     def do(self, population):
         raise NotImplementedError
 
+
 class OperonReinserter(Reinserter):
-    def __init__(self,pool,method,comparision_size):
+    def __init__(self, pool_list, method, comparision_size,to_type):
         super().__init__()
+        self.to_type = to_type
+        self.pool_list = pool_list
         self.comparision_size = comparision_size
         self.method = method
-        self.pool = pool
 
-
-    def do(self,population):
-        if self.comparision_size>population.pop_size:
+    def do(self, population):
+        rng = Operon.RomuTrio(random.randint(1, 1000000))
+        if self.comparision_size > population.pop_size:
             raise ValueError("比较数量大于种群数量")
-        if not isinstance(self.comparision_size,int):
+        if not isinstance(self.comparision_size, int):
             raise ValueError("比较数量必须为int类型")
-        if self.method=="ReplaceWorst":
-            rein=Operon.ReplaceWorstReinserter(self.comparision_size)
-        elif self.method=="KeepBest":
-            rein=Operon.KeepBestReinserter(self.comparision_size)
+        if self.method == "ReplaceWorst":
+            rein = Operon.ReplaceWorstReinserter(self.comparision_size)
+        elif self.method == "KeepBest":
+            rein = Operon.KeepBestReinserter(self.comparision_size)
         else:
             raise ValueError("reinserter方法选择错误")
-        if population.pop_type=="Operon":
-            if len(population.target_pop_list)!=len(population.target_fit_list):
+        if population.pop_type == "Operon":
+            ind_list=[]
+            if len(population.target_pop_list) != len(population.target_fit_list):
                 raise ValueError("个体与适应度数量不符")
+            for i in range(len(population.target_pop_list)):
+                ind=Operon.Individual()
+                ind.Genotype=population.target_pop_list[i]
+                ind.Setfitness([population.target_fit_list[i]],0)
+            rein(rng,ind_list,self.pool_list)
+            new_target_pop_list=[]
+            new_target_fitness_list=[]
+            for i in ind_list:
+                new_target_pop_list.append(i.Genotype)
+                new_target_fitness_list.append(i.GetFitness(0)[0])
+            population.target_pop_list=new_target_pop_list
+            population.target_fit_list=new_target_pop_list
+            if self.to_type!="Operon":
+                pass
+
         else:
             pass
-
-
-
-
--- keplar/operator/selector.py
+++ keplar/operator/selector.py
@@ -7,6 +7,7 @@ from bingo.symbolic_regression import AGraph
 from keplar.population.individual import Individual
 from keplar.operator.operator import Operator
 from keplar.population.population import Population
+import pyoperon as Operon
 
 
 class Selector(Operator):
@@ -28,9 +29,9 @@ class BingoSelector(Operator):
             raise ValueError("淘汰比例数值错误")
 
     def do(self, population):
-        target_pop_size = int(population.get_pop_size() * (1-self.eliminate_p))
+        target_pop_size = int(population.get_pop_size() * (1 - self.eliminate_p))
         if self.method == "tournament":
-            selector = Tournament(int(population.get_pop_size()/10))
+            selector = Tournament(int(population.get_pop_size() / 10))
         elif self.method == "age_fitness":
             selector = AgeFitness(10)
         elif self.method == "deterministic_crowding":
@@ -41,7 +42,7 @@ class BingoSelector(Operator):
         for i in population.pop_list:
             equation = i.equation
             bingo_ind = AGraph(equation=str(equation))
-            bingo_ind.fitness=i.get_fitness()
+            bingo_ind.fitness = i.get_fitness()
             bingo_ind._update()
             bingo_pop.append(bingo_ind)
         new_bingo_pop = selector(population=bingo_pop, target_population_size=target_pop_size)
@@ -52,3 +53,22 @@ class BingoSelector(Operator):
             new_pop_list.append(ind)
         new_pop.initial(new_pop_list)
         return new_pop
+
+
+class OperonSelector(Selector):
+    def __init__(self, method, select_size):
+        super().__init__()
+        self.select_size = select_size
+        self.method = method
+
+    def do(self, population):
+        if self.method=="TournamentSelector":
+            sel=Operon.TournamentSelector()
+        elif self.method=="RankTournamentSelector":
+            sel=Operon.RankTournamentSelector()
+        elif self.method=="ProportionalSelector":
+            sel=Operon.ProportionalSelector()
+        elif self.method=="RandomSelector":
+            sel=Operon.RandomSelector()
+        else:
+            raise ValueError("selector方法输入错误")
