b163d75bef4b102df9f38173d3d69fa1d432b6db
tzh <284171810@qq.com>
2023-06-09 16:29:21 +0800

    ccc

--- keplar/population/individual.py
+++ keplar/population/individual.py
@@ -9,6 +9,12 @@ class Individual:
     def format(self):
         for i in self.func:
             tk = int(i)
+            if i<2000:
+
+            elif i>2000 and i<3000:
+            elif i>3000:
+            else:
+                raise ValueError(f"编码无意义{i}")
 
     def reset_equation(self, new_equation):
         self.equation = new_equation
