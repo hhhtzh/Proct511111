c93f69da70b0f8908a57794d8d3dd9549d8db00e
tzh <284171810@qq.com>
2023-07-03 14:56:30 +0800

    tbz

--- operonbingo_main.py
+++ operonbingo_main.py
@@ -7,11 +7,11 @@ from keplar.operator.crossover import OperonCrossover
 from keplar.operator.evaluator import OperonEvaluator
 from keplar.operator.mutation import OperonMutation
 from keplar.operator.selector import BingoSelector
-#
-# data = Data("txt", "datasets/1.txt", ["x", "y"])
-data=Data("pmlb","1027_ESL",["x1","x2","x3",'y'])
+
+data = Data("txt", "datasets/1.txt", ["x", "y"])
+# data=Data("pmlb","1027_ESL",["x1","x2","x3",'y'])
 data.read_file()
-# data.set_xy("y")
+data.set_xy("y")
 x = data.get_np_x()
 y = data.get_np_y()
 creator = OperonCreator("balanced", x, y, 1000, "Operon")
