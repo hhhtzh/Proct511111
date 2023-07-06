1e72d55f26a56129440591606381d636f0ed3ed4
tzh <284171810@qq.com>
2023-07-06 14:50:23 +0800

    tbz

---
+++ draw_main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from keplar.draw.BoxImg import draw_box_diagram, addLog
+
+Vladislavleva = pd.read_csv('result/Vladislavleva_result.csv')
+draw_box_diagram(addLog(Vladislavleva['GpBingo']), addLog(Vladislavleva['Bingo']), addLog(Vladislavleva['OperonBingo']),
+                 addLog(Vladislavleva['Operon']), addLog(Vladislavleva['MTaylorGp']), addLog(Vladislavleva['MTaylorBingo']),
+                 addLog(Vladislavleva['MTaylorOperon']), addLog(Vladislavleva['TaylorBingo']), addLog(Vladislavleva['BingoCPP']), addLog(Vladislavleva['UDSR']),
+                 "vlsdislavleva", isAll=True)
+
+# draw_box_diagram(addLog(GECCO['TaylorGP']), addLog(GECCO['GPLearn']), addLog(GECCO['FFX']),
+#                  addLog(GECCO['LinearRegression']), addLog(GECCO['KernelRidge']), addLog(GECCO['RF']),
+#                  addLog(GECCO['SVM']), addLog(GECCO['XGBoost']), addLog(GECCO['GSGP']), addLog(GECCO['BSR']),
+#                  "GECCO", isAll=False)
+
+# draw_box_diagram_inside(addLog(GECCO['TaylorGP']), addLog(GECCO['GPLearn']), addLog(GECCO['FFX']),
+#                         addLog(GECCO['LinearRegression']), addLog(GECCO['KernelRidge']), addLog(GECCO['RF']),
+#                         addLog(GECCO['SVM']), addLog(GECCO['XGBoost']), addLog(GECCO['GSGP']), addLog(GECCO['BSR']),
+#                         "GECCO")
