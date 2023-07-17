f688940f633c8bdc5914b48cb7d8046509f7e83d
tzh <284171810@qq.com>
2023-07-17 15:28:58 +0800

    tbz

--- gp_main.py
+++ gp_main.py
@@ -1,3 +1,4 @@
 from gplearn.genetic import SymbolicRegressor
 
-sr=SymbolicRegressor(population_size=128,generations=1000)
+sr = SymbolicRegressor(population_size=128, generations=1000,
+                       function_set=["add", "sub", "mul", "div", "sin", "exp", "cos", 'sqrt', 'log', 'sin', 'pow', 'exp', '^'])
