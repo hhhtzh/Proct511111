d8e1ea04b994013810e43edf1cde82ca95935109
tzh <284171810@qq.com>
2023-08-29 14:01:22 +0800

    tbz

--- help9.py
+++ help9.py
@@ -9,13 +9,14 @@ from keplar.operator.evaluator import SingleBingoEvaluator, OperonSingleEvaluato
 # data = Data("txt_pmlb", "datasets/pmlb/val/529_pollen.txt",
 #             ["x0", "x1", "x2", "x3", "y"])
 # data = Data("txt_pmlb", "datasets/feynman/train/feynman-bonus.8.txt", ["x0", "x1", "y"])
-data = Data("txt_pmlb", "datasets/pmlb/train/588_fri_c4_1000_100.txt", ["x0", "x1", "x2", "x3", "y"])
+# data = Data("txt_pmlb", "datasets/pmlb/train/588_fri_c4_1000_100.txt", ["x0", "x1", "x2", "x3", "y"])
+data = Data("txt_pmlb", "/home/tzh/PycharmProjects/pythonProjectAR5/datasets/pmlb/val/620_fri_c1_1000_25.txt", ["x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24","y"])
 data.read_file()
 data.set_xy("y")
 # npx=data.get_np_x()
 # print(npx)
 fit_list = []
-with open('result/pmlb_588_fri_c4_1000_100_equ.csv', 'rt') as csvfile:
+with open('result/pmlb_294_satellite_image_equ.csv', 'rt') as csvfile:
     reader = csv.reader(csvfile)
     rows = [row for row in reader]
 i = 1
@@ -28,4 +29,4 @@ while i < len(rows):
     fit_list.append(fit)
     i += 1
 fit_pd = pd.DataFrame({'MTaylor(with new sparse)': fit_list})
-fit_pd.to_csv(r"result/pmlb_588_fri_c4_1000_100_fit.csv", sep=',', mode="a")
+fit_pd.to_csv(r"result/pmlb_620_fri_c1_1000_25_fit.csv", sep=',', mode="a")
