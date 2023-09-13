d2887f439767be55f3f3c00afca17f3316158dcb
tzh <284171810@qq.com>
2023-09-13 11:04:41 +0800

    tbz

--- keplar/draw/write_in_feature.py
+++ keplar/draw/write_in_feature.py
@@ -33,16 +33,16 @@
 
 import pandas as pd
 
-os.()
+# os.()
 
 
 from bingo.symbolic_regression.agraph.string_parsing import eq_string_to_infix_tokens
 
 # 读取现有的 feather 文件
 ft = pd.read_feather("pmlb_results.feather")
-time = pd.read_csv("../../zjw_result/503_wind_time.csv")
-equ = pd.read_csv("../../zjw_result/503_wind_equ.csv")
-fit = pd.read_csv("../../zjw_result/503_wind_R2.csv")
+time = pd.read_csv("../../zjw_result/608_fri_c3_1000_10_time.csv")
+equ = pd.read_csv("../../zjw_result/608_fri_c3_1000_10_equ.csv")
+fit = pd.read_csv("../../zjw_result/608_fri_c3_1000_10_R2.csv")
 print(equ)
 time1 = time.iloc[:, -1]
 equ1 = equ.iloc[:, -1]
@@ -58,7 +58,7 @@ for i in range(len(time1)):
             ms += 1
     print(float(fit1[i]))
     new_row_data = {
-        "dataset": "503_wind",
+        "dataset": "612_fri_c1_1000_10",
         "algorithm": "mtaylor",
         "model_size": ms,
         "training time (s)": float(time1[i]),
